f2f90b03aa42fe4bca30aa0b1e3da67fbece036f
nisen <nis@16care.com>
2018-05-14 10:48:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is solutions of [go koans](https://github.com/cdarwin/go-koans)
---
+++ about_common_interfaces.go
@@ -0,0 +1,35 @@
+package go_koans
+
+import "bytes"
+import "strings"
+
+func aboutCommonInterfaces() {
+	{
+		in := new(bytes.Buffer)
+		in.WriteString("hello world")
+
+		out := new(bytes.Buffer)
+
+		/*
+		   Your code goes here.
+		   Hint, use these resources:
+
+		   $ godoc -http=:8080
+		   $ open http://localhost:8080/pkg/io/
+		   $ open http://localhost:8080/pkg/bytes/
+		*/
+		out = in
+
+		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
+	}
+
+	{
+		in := new(bytes.Buffer)
+		in.WriteString("hello world")
+
+		out := new(bytes.Buffer)
+		out.WriteString(strings.Split(in.String(), " ")[0])
+
+		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
+	}
+}
